3179d6725bfac0579aeeb7f50c2744407b333630
Blissmode <krithickrockz@gmail.com>
2018-03-21 05:07:58 +0530

    Partly implemented the file upload. Minor design changes.
    	modified:   package.json
    	modified:   public/index.html
    	modified:   src/App.js
    	modified:   src/App.test.js
    	new file:   src/fileUpload.js
    	modified:   src/index.js
    	modified:   yarn.lock

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,18 @@ import React, { Component } from 'react';
 import Samples from './components/samples';
 import './App.css';
 
+
 class App extends Component {
   render() {
+    var myStyle={
+      fontSize:60,
+      color: "blue",
+    }
+
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to DigiCon</h1>
+          <h1 style={myStyle} className="App-title">Welcome to DigiCon</h1>
           <Samples />
         </header>
       </div>
