235860634181d4b23210273105bc6f8d0f70041b
aman7103 <aman.kr7103@gmail.com>
2018-03-20 23:44:09 +0530

    added image upload interface

---
+++ frontend/src/upload.py
@@ -0,0 +1,39 @@
+from flask import Flask, url_for, send_from_directory, request
+import time
+import logging, os
+from werkzeug import secure_filename
+
+app = Flask(__name__)
+file_handler = logging.FileHandler('server.log')
+app.logger.addHandler(file_handler)
+app.logger.setLevel(logging.INFO)
+
+PROJECT_HOME = os.path.dirname(os.path.realpath(__file__))
+UPLOAD_FOLDER = '{}/image'.format(PROJECT_HOME)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def create_new_folder(local_dir):
+    newpath = local_dir
+    if not os.path.exists(newpath):
+        os.makedirs(newpath)
+    return newpath
+
+@app.route('/', methods = ['POST'])
+
+def api_root():
+    app.logger.info(PROJECT_HOME)
+    if request.method == 'POST' and request.files['image']:
+    	app.logger.info(app.config['UPLOAD_FOLDER'])
+    	img = request.files['image']
+    	img_name = time.strftime("%Y%m%d-%H%M%S")
+    	create_new_folder(app.config['UPLOAD_FOLDER'])
+    	saved_path = os.path.join(app.config['UPLOAD_FOLDER'], img_name)
+    	app.logger.info("saving {}".format(saved_path))
+    	img.save(saved_path)
+    	return send_from_directory(app.config['UPLOAD_FOLDER'],img_name, as_attachment=True)
+    else:
+    	return "Where is image?"
+
+if __name__ == '__main__':
+         app.run(host='0.0.0.0', debug=False)
