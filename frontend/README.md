abacfffbd1e63cd18d660b3acdf3bf9f53d76b9f
Naresh <ghostwriternr@gmail.com>
2018-03-22 18:18:40 +0530

    Clean up React

--- frontend/README.md
+++ frontend/README.md
@@ -1,1 +1,8 @@
 # Frontend for OpenSoft 2018 project - DigiCon
+
+## Instructions to run the frontend
+
+- Install [nodejs (will also install npm)](https://nodejs.org/en/download/package-manager/)
+- Install [yarn](https://yarnpkg.com/lang/en/docs/install/)
+- Go to the frontend directory and run `yarn`. This will install all dependencies.
+- Start the React server using `yarn start`.
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Samples from './components/samples';
 import './App.css';
-import SimpleReactFileUpload from './fileUpload'
+import SimpleReactFileUpload from './fileUpload';
 
-class App extends Component {
-  render() {
-    var headerStyle={
-      fontSize:60,
-      color: "blue",
-    }
-
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 style={headerStyle} className="App-title">Welcome to DigiCon</h1>
-          <Samples />
-          <SimpleReactFileUpload />
-        </header>
+const App = () => (
+  <div className="App">
+    <div className="App-welcome">
+      <div className="welcome-text">
+        DigiCon
+        <div className="App-description">
+          It is hard to change a doctor's handwriting. But it shouldn't be hard to read them.
+        </div>
       </div>
-    );
-  }
-}
+      <div className="welcome-bottom" />
+    </div>
+    <header className="App-main">
+      <h1 className="App-title">
+        Welcome to DigiCon
+      </h1>
+      <Samples />
+      <SimpleReactFileUpload />
+    </header>
+  </div>
+);
 
 export default App;
