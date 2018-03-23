2a6e9edc7a7c69271fd74a74089e7f7639c349ff
Dibya Das <dibyadascool@gmail.com>
2018-03-24 02:17:03 +0530

    refactored upload method

--- backend/app.py
+++ backend/app.py
@@ -2,24 +2,23 @@
 """
 Main flask process
 """
-from flask import Flask, url_for, send_from_directory, request, jsonify
-from flask_cors import CORS
-import requests
-import utils
-import imghdr
+from flask import Flask, url_for, send_from_directory, request
 import time
 import logging, os
+from werkzeug import secure_filename
+import requests
 
-APP = Flask(__name__)
-CORS(APP)
+app = Flask(__name__)
 REQUESTS_SESSION = requests.Session()
 file_handler = logging.FileHandler('server.log')
-APP.logger.addHandler(file_handler)
-APP.logger.setLevel(logging.INFO)
+app.logger.addHandler(file_handler)
+app.logger.setLevel(logging.INFO)
 
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 PROJECT_HOME = os.path.dirname(os.path.realpath(__file__))
 UPLOAD_FOLDER = '{}/image'.format(PROJECT_HOME)
-APP.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 
 def create_new_folder(local_dir):
     newpath = local_dir
@@ -27,29 +26,35 @@ def create_new_folder(local_dir):
         os.makedirs(newpath)
     return newpath
 
-@APP.route('/', methods=['GET'])
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET'])
 def index():
     """
     Handle http request to root
     """
     return "Hello!"
 
-@APP.route('/upload', methods = ['POST'])
+@app.route('/upload', methods = ['POST'])
 def api_root():
-    APP.logger.info(PROJECT_HOME)
-    if request.method == 'POST' and request.files['image']:
-        APP.logger.info(APP.config['UPLOAD_FOLDER'])
+    app.logger.info(PROJECT_HOME)
+    try:
         img = request.files['image']
-        imgname = time.strftime("%Y%m%d-%H%M%S")
-        x = imghdr.what(img)
-        img_name=imgname+'.'+x
-        create_new_folder(APP.config['UPLOAD_FOLDER'])
-        saved_path = os.path.join(APP.config['UPLOAD_FOLDER'], img_name)
-        APP.logger.info("saving {}".format(saved_path))
-        img.save(saved_path)
-        return send_from_directory(APP.config['UPLOAD_FOLDER'],img_name, as_attachment=True)
-    else:
-        return {'error': 'bad request'}
+        if request.method == 'POST' and img and allowed_file(img.filename):
+            app.logger.info(app.config['UPLOAD_FOLDER'])
+            img = request.files['image']
+            create_new_folder(app.config['UPLOAD_FOLDER'])
+            saved_path = os.path.join(app.config['UPLOAD_FOLDER'], img.filename)
+            app.logger.info("saving {}".format(saved_path))
+            img.save(saved_path)
+            return ("Image uploaded successfully", 200, {})
+        else:
+            return ("No image sent", 401, {})
+    except Exception as e:
+        app.logger.info(e)
+        return ("Error occured:- "+str(e), 400, {})
 
 if __name__ == "__main__":
-    APP.run(debug=True, host='0.0.0.0', port=8080, use_reloader=False)
+    app.run(debug=True, host='0.0.0.0', port=8080, use_reloader=False)
