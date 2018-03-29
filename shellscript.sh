32a5351ff64229515a23fc4d029bcfd00283daed
TheLethalCode <kousshikraj.raj@gmail.com>
2018-03-29 22:28:46 +0530

    Update shellscript

--- shellscript.sh
+++ shellscript.sh
@@ -1,7 +1,7 @@
 curl -fsSL https://get.docker.com | sh
 mkdir -p /etc/systemd/system/docker.service.d
 touch /etc/systemd/system/docker.service.d/http-proxy.conf
-echo -e "[Service]\nEnvironment=\"HTTP_PROXY=http://172.16.2.30:port/\"" >> /etc/systemd/system/docker.service.d/http-proxy.conf
+echo -e "[Service]\nEnvironment=\"HTTP_PROXY=http://172.16.2.30:8080/\"" >> /etc/systemd/system/docker.service.d/http-proxy.conf
 systemctl daemon-reload
 systemctl restart docker
 apt install docker-compose
