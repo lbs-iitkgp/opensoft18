36c38b711b0dc341bcf660154d4558def98a82f0
Naresh <ghostwriternr@gmail.com>
2018-03-30 23:32:49 +0530

    Fix errors in corenlp frontend

--- frontend/src/components/corenlp.jsx
+++ frontend/src/components/corenlp.jsx
@@ -8,47 +8,48 @@ const Corenlp = (props) => {
   const { nlpData } = props;
 
   const tableData = [];
-  nlpData.map((key, idx) => {
+  nlpData.forEach((key, idx) => {
     if (key) {
-      switch(idx) {
+      switch (idx) {
         case 0: tableData.push({
-                  type: "Hospital",
-                  value: key,
-                });
-                break;
+          type: 'Hospital',
+          value: key,
+        });
+          break;
         case 1: tableData.push({
-                  type: "Doctor",
-                  value: key,
-                });
-                break;
+          type: 'Doctor',
+          value: key,
+        });
+          break;
         case 2: tableData.push({
-                  type: "Address",
-                  value: key,
-                });
-                break;
+          type: 'Address',
+          value: key,
+        });
+          break;
         case 3: tableData.push({
-                  type: "Specialization",
-                  value: key,
-                });
-                break;                                                
+          type: 'Specialization',
+          value: key,
+        });
+          break;
         case 4: tableData.push({
-                  type: "Contact details",
-                  value: key,
-                });
-                break;
+          type: 'Contact details',
+          value: key,
+        });
+          break;
         case 5: tableData.push({
-                  type: "Email address",
-                  value: key,
-                });
-                break;
+          type: 'Email address',
+          value: key,
+        });
+          break;
         case 6: tableData.push({
-                  type: "Date",
-                  value: key,
-                });
-                break;                                                                
+          type: 'Date',
+          value: key,
+        });
+          break;
+        default: break;
       }
     }
-  })
+  });
 
   const columns = [{
     Header: 'Entity type',
